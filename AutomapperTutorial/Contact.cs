0754bf4e058f98be887453aa81845480d4c591a2
ibrahim <ibrahim.mohamed738@gmail.com>
2023-01-12 15:42:14 +0300

    Add project files.

---
+++ AutomapperTutorial/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace AutomapperTutorial
+{
+	public class Contact
+	{
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+		public string PlaceOfBirth { get; set; } = string.Empty;
+		public DateTime DateAdded { get; set; }
+        public DateTime? DateModified { get; set; }
+    }
+}
---
+++ AutomapperTutorial/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutomapperTutorial.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ContactController : ControllerBase
+	{
+		private List<Contact> contacts = new List<Contact> 
+		{
+			new Contact { 
+				Id = 1,
+				Name = "Ibrahim Mohamed",
+				FirstName = "Ibrahim",
+				LastName = "Mohamed",
+				PlaceOfBirth = "Burco",
+				DateAdded = new DateTime (2023,01,01),
+				DateModified = null
+			},
+
+			new Contact {
+				Id = 2,
+				Name = "Ahmed Mohamed",
+				FirstName = "Ahmed",
+				LastName = "Mohamed",
+				PlaceOfBirth = "Dubai",
+				DateAdded = new DateTime (2023,01,04),
+				DateModified = null
+			}
+		};
+
+		[HttpGet]
+		public ActionResult<List<Contact>> GetContacts()
+		{
+			return contacts;
+		}
+	}
+}
